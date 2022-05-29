003d2ff91b0fcd7a76eab45df3c580bb1ae4ceb9
Prajval <prajvalhl@gmail.com>
2022-05-29 14:03:01 +0530

    initial setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+// vuesweetalert2
+import VueSweetalert2 from "vue-sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+
+//firebase
+import "firebase/auth";
+import firebaseConfig from "./utils/config";
+import { initializeApp } from "@firebase/app";
+initializeApp(firebaseConfig);
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+app.use(VueSweetalert2);
+app.mount("#app");
---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Xy4wBSIAOP3ypgszd7AL-KP0SdLVNLs",
+  authDomain: "git-app-c8f1f.firebaseapp.com",
+  projectId: "git-app-c8f1f",
+  storageBucket: "git-app-c8f1f.appspot.com",
+  messagingSenderId: "721043599581",
+  appId: "1:721043599581:web:d8a55708878170ba38ab34",
+};
+
+export default firebaseConfig;
